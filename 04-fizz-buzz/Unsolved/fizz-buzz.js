5bb05c386848e81c896baf4e9888d50edb0847cd
Sandra <sandra.alpha2010@gmail.com>
2020-10-20 22:31:14 -0700

    finished fizzbuzz example with some notes to self as to why a different solution doesn't quite work

--- 04-fizz-buzz/Unsolved/fizz-buzz.js
+++ 04-fizz-buzz/Unsolved/fizz-buzz.js
@@ -4,4 +4,34 @@
 // If a number is evenly divisible by both 3 AND 5, print "Fizz Buzz"
 // If a number is not divisible by 3 or 5, print the number
 
-var fizzBuzz = function(arr) {};
+var fizzBuzz = function (arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] % 3 === 0 && arr[i] % 5 === 0) {
+            console.log("Fizz Buzz");
+        } else if (arr[i] % 5 === 0) {
+            console.log("Buzz");
+        } else if (arr[i] % 3 === 0) {
+            console.log("Fizz");
+        } else {
+            console.log(arr[i]);
+        }
+    }
+};
+
+/*
+
+Note: this condition kind of works, except for number 15, number 15 is divisible by 3 AND 5, but
+the way it's set up here causes 15 to pass the first condition instead and print a "Fizz".
+The AND condition needs to come first to catch any numbers that fit both conditions.
+
+if (arr[i] % 3 === 0) {
+            console.log("Fizz");
+        } else if (arr[i] % 5 === 0) {
+            console.log("Buzz");
+        } else if (arr[i] % 3 === 0 && arr[i] % 5 === 0) {
+            console.log("Fizz Buzz");
+        } else {
+            console.log(arr[i]);
+        }
+
+*/
