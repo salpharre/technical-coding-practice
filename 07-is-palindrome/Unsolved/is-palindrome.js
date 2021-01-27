48506e1f5176b75dadfb3f966c6f7ae9d7e2e2f4
Sandra <sandra.alpha2010@gmail.com>
2021-01-26 18:07:21 -0800

    is palindrome done

--- 07-is-palindrome/Unsolved/is-palindrome.js
+++ 07-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,23 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+var isPalindrome = function(str) {
+let lowerCaseStr = str.toLowerCase();
+let splitStr = lowerCaseStr.split("");
+let reverseStr = splitStr.reverse();
+let isPalindromeStr = reverseStr.join("");
+
+if (isPalindromeStr === lowerCaseStr) {
+    return true;
+} else {
+    return false;
+}
+
+};
+/*
+palindrome: naan, eye, noon
+
+split string, reverse it, put it back together and then compare to original
+if it's the same = true
+if it's not = false
+
+*/
